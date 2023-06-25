eead9c8e6ad4e02c47ff0b806a56da96c695ebe8
tatii0801 <tatiananieva0801@gmail.com>
2023-06-25 06:41:25 -0300

    modificaciones para agregar listar y llamar

--- frontend/angular/src/app/app-routing.module.ts
+++ frontend/angular/src/app/app-routing.module.ts
@@ -3,10 +3,18 @@ import { RouterModule, Routes } from '@angular/router';
 
 import { RecetaComponent } from './components/receta/receta.component';
 import { RecetaFormComponent } from './components/receta-form/receta-form.component';
+import { ProductoComponent } from './components/producto/producto.component';
+import { PagoComponent } from './components/pago/pago.component';
+
 
 const routes: Routes = [
+  { path: 'producto', component: ProductoComponent },
   { path: 'receta-form/:id', component: RecetaFormComponent },
   { path: 'receta', component: RecetaComponent },
+  { path: 'pago', component: PagoComponent },
+
+  { path: '**', pathMatch:'full',redirectTo:'producto' }
+
 ];
 
 @NgModule({
