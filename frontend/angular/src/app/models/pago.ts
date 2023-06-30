db82ff80542f378315ba354b615321fd6fe2df57
MerlosDamian713 <sdmerlos@hotmail.com>
2023-06-30 15:10:21 -0300

    Merge branch 'master' of https://github.com/MartinSGuzman/WinchesterWeb

--- frontend/angular/src/app/models/pago.ts
+++ frontend/angular/src/app/models/pago.ts
@@ -1,9 +1,13 @@
+import { Items } from "./items";
+import { Receta } from "./receta";
+
 export class Pago {
     _id!:string;
     monto!: number;
+    receta!:Receta;
+    items!:Items;
     metodo!:string;
-    articulo!:string;
     nota!:string;
-    hora!:string;
-    dia!:string;
+    horario!:string;
+    fecha!:Date;
 }
