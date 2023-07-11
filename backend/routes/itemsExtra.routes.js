18e463764bb7b0b8061fb9c0fdfb4c48b31f467b
Brian0403 <brayflo515@gmail.com>
2023-07-11 16:50:50 -0300

    ultimo push ahora si XD

--- backend/routes/itemsExtra.routes.js
+++ backend/routes/itemsExtra.routes.js
@@ -1,13 +1,14 @@
 //defino controlador 
 const itemCtrl = require('./../controllers/itemsExtra.controller.js');
+const authCtrl = require('./../controllers/auth.controller.js');
 //creamos el manejador de rutas
 const express = require('express');
 const router = express.Router();
 //definimos las rutas para la gestion
-router.get('/', itemCtrl.getItems);
-router.post('/post', itemCtrl.createItem);
-router.get('/:id', itemCtrl.getItem);
-router.put('/:id', itemCtrl.editItem);
-router.delete('/:id', itemCtrl.deleteItem);
+router.get('/',authCtrl.verifyToken, itemCtrl.getItems);
+router.post('/post',authCtrl.verifyToken, itemCtrl.createItem);
+router.get('/:id',authCtrl.verifyToken, itemCtrl.getItem);
+router.put('/:id',authCtrl.verifyToken, itemCtrl.editItem);
+router.delete('/:id',authCtrl.verifyToken, itemCtrl.deleteItem);
 //exportamos el modulo de rutas
 module.exports = router;
--- backend/routes/pedido.routes.js
+++ backend/routes/pedido.routes.js
@@ -1,13 +1,14 @@
 //defino controlador 
 const PedidoCtrl = require('./../controllers/pedido.controller.js');
+const authCtrl = require('./../controllers/auth.controller.js');
 //creamos el manejador de rutas
 const express = require('express');
 const router = express.Router();
 //definimos las rutas para la gestion
-router.get('/', PedidoCtrl.getPedidos);
-router.post('/post', PedidoCtrl.createPedido);
-router.get('/:id', PedidoCtrl.getPedido);
-router.put('/:id', PedidoCtrl.editPedido);
-router.delete('/:id', PedidoCtrl.deletePedido);
+router.get('/',authCtrl.verifyToken, PedidoCtrl.getPedidos);
+router.post('/post',authCtrl.verifyToken, PedidoCtrl.createPedido);
+router.get('/:id',authCtrl.verifyToken, PedidoCtrl.getPedido);
+router.put('/:id',authCtrl.verifyToken, PedidoCtrl.editPedido);
+router.delete('/:id',authCtrl.verifyToken, PedidoCtrl.deletePedido);
 //exportamos el modulo de rutas
 module.exports = router;
